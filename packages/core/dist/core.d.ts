e9d2cf7a9f954cca07001c571dfa0c1c6cc51d69
Brandon Peterson <brandon@objectstudio.co>
2018-05-11 13:01:27 -0700

    * Ignore .vs * Ignore / remove dist folder

--- packages/core/dist/core.d.ts
+++
@@ -1,9 +0,0 @@
-import { CacheService } from "./cache/cache.service";
-import { LogService } from "./logging/log.service";
-import { AlertsService } from "./alerts/alerts.service";
-export declare class Core {
-    log: LogService;
-    cache: CacheService;
-    alerts: AlertsService;
-    constructor(log: LogService, cache: CacheService, alerts: AlertsService);
-}
--- packages/core/dist/logging/console-log.provider.d.ts
+++
@@ -1,8 +0,0 @@
-import { ILogProvider } from "./log.service";
-/**
- * A console log implementation for the logging service.
- */
-export declare class ConsoleLogProvider implements ILogProvider {
-    constructor();
-    write(level: string, args: any[]): void;
-}
